4fa68bd508810ca7fe494381c05063e1b6a34fb2
omaracrystal <omaracrystal@gmail.com>
2015-10-04 17:58:23 -0600

    set up angular 2

--- Unit-2-App/js/app.js
+++ Unit-2-App/js/app.js
@@ -1,6 +1,6 @@
 //declare angular app and all it's dependencies
 
-var app = angular.module('whateverYourModuleNameIs', ['ngRoute']);
+var app = angular.module("yourAppName", ['ngRoute']);
 
 
 app.config(function($routeProvider) {
